64c7a892b3c974f4f548942bbfb0bc3452d5af1d
Pontificust <lihtinmann88@yandex.ru>
2026-09-02 17:36:51 +0300

    feat: the caesarCipher() is tested and implemented

---
+++ src/modules/caesarCipher/caesarCipher.js
@@ -0,0 +1,39 @@
+export const caesarCipher = (str, shiftFactor) => {
+  let encryptedStr = "";
+  
+  const isUpperCase = (charCode) => {
+      return charCode <= 90 && charCode >= 65;
+    };
+    
+    const isLowerCase = (charCode) => {
+        return charCode >= 97 && charCode <= 122;
+    };
+    
+    const getShiftedLetter = (shiftedIdx, maxIdx) => {
+        if (shiftedIdx <= maxIdx) {
+            return String.fromCharCode(shiftedIdx);
+        } else {
+            return String.fromCharCode(shiftedIdx - 26);
+        }
+    };
+    
+    const mod = (n, m) => {
+        return ((n % m) + m) % m;
+    };
+    const shift = mod(shiftFactor, 26);
+
+  for (let i = 0; i < str.length; i += 1) {
+    const unshiftedIdx = str.charCodeAt(i);
+    const shiftedIdx = unshiftedIdx + shift;
+
+    if (isUpperCase(unshiftedIdx)) {
+      encryptedStr += getShiftedLetter(shiftedIdx, 90);
+    } else if(isLowerCase(unshiftedIdx)){
+      encryptedStr += getShiftedLetter(shiftedIdx, 122);
+    } else {
+        encryptedStr += str[i];
+    }
+  }
+
+  return encryptedStr;
+};
---
+++ src/modules/caesarCipher/caesarCipher.test.js
@@ -0,0 +1,27 @@
+import { test, expect } from '@jest/globals';
+import { caesarCipher } from './caesarCipher.js';
+
+
+test('returns an input string encrypted', () => {
+    expect(caesarCipher('HELLO', 3)).toEqual('KHOOR');
+});
+
+test('returns an input string encrypted', () => {
+    expect(caesarCipher('HeLLo', 3)).toEqual('KhOOr');
+});
+
+test('wrapping from z to a', () => {
+    expect(caesarCipher('xyz', 3)).toEqual('abc');
+});
+
+test('punctuation must be saved', () => {
+    expect(caesarCipher('Hello, World!', 3)).toEqual('Khoor, Zruog!');
+});
+
+test('negative shift', () => {
+    expect(caesarCipher('hello', -10)).toEqual('xubbe');
+});
+
+test('shift is bigger than 26', () => {
+    expect(caesarCipher('hello', 55)).toEqual('khoor');
+});
