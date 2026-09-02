982b1b7829176672aa097d7137eed28e708f8f16
Pontificust <lihtinmann88@yandex.ru>
2026-09-02 13:15:19 +0300

    feat: capitalize() tested and implemented

---
+++ src/modules/capitalize/capitalize.js
@@ -0,0 +1,6 @@
+export const capitalize = (str) => {
+    if(typeof str !== 'string') {
+        throw new Error('wrong type, please input a string value'); 
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
