5e6ce7043da6094bb3cedf49e463ae26ce3bd52a
Pontificust <lihtinmann88@yandex.ru>
2026-09-02 16:11:57 +0300

    feat: the Calculator is tested and implemented

---
+++ src/modules/calculator/Calculator.js
@@ -0,0 +1,31 @@
+export class Calculator {
+    static checkType(a, b) {
+        if(!Number.isFinite(a) || !Number.isFinite(b)){
+            throw new Error('wrong type, please input a numeric value');
+        }
+    };
+
+    static add(a, b) {
+        this.checkType(a, b);
+        return Math.round((a + b) * 100) / 100;
+    };
+
+    static substract(a, b) {
+        this.checkType(a, b);
+        return Math.round((a - b) * 100) / 100;
+    };
+
+    static multiply(a, b) {
+        this.checkType(a, b);
+        return Math.round(a * b * 100) / 100;
+    };
+
+    static divide(a, b) {
+        this.checkType(a, b);
+
+        if(b === 0) {
+            throw new Error('a division by zero is impossible');
+        }
+        return Math.round(a / b * 100) / 100;
+    };
+};
---
+++ src/modules/calculator/calculator.test.js
@@ -0,0 +1,98 @@
+import { expect, test } from '@jest/globals';
+import { Calculator } from './Calculator.js';
+
+test('add() returns a correct addition of two integers', () => {
+    expect(Calculator.add(1, 2)).toEqual(3);
+});
+
+test('add() returns a correct addition of two integers', () => {
+    expect(Calculator.add(1234, 2675)).toEqual(3909);
+});
+
+test('multiply() retruns a corrrect multiplication of input integers', () => {
+    expect(Calculator.multiply(2, 3)).toEqual(6);
+});
+
+test('multiply() retruns a correct multiplication of input integers', () => {
+    expect(Calculator.multiply(1902, 3)).toEqual(5706);
+});
+
+test('substract() retruns a corrrect subtraction of input integers', () => {
+    expect(Calculator.substract(1, 2)).toEqual(-1);
+});
+
+test('substract() retruns a correct subtraction of input integers', () => {
+    expect(Calculator.substract(5000, 1999)).toEqual(3001);
+});
+
+test('add() returns a correct addition of two floats', () => {
+    expect(Calculator.add(1.2, 2.46)).toEqual(3.66);
+});
+
+test('add() returns a correct addition of two floats', () => {
+    expect(Calculator.add(1234.444, 2675.3245)).toEqual(3909.77);
+});
+
+test('multiply() retruns a correct multiplication of input floats', () => {
+    expect(Calculator.multiply(2.3, 3.5)).toEqual(8.05);
+});
+
+test('multiply() retruns a correct multiplication of input floats', () => {
+    expect(Calculator.multiply(1902.99, 3.99)).toEqual(7592.93);
+});
+
+test('substract() retruns a correct subtraction of input floats', () => {
+    expect(Calculator.substract(1.22, 2.99)).toEqual(-1.77);
+});
+
+test('substract() retruns a correct subtraction of input floats', () => {
+    expect(Calculator.substract(5000.9324, 1999.343224)).toEqual(3001.59);
+});
+
+test('divide() retruns a correct division of input integers(exact division)', () => {
+    expect(Calculator.divide(10, 5)).toEqual(2);
+});
+
+test('divide() retruns a correct division of input integers(exact division)', () => {
+    expect(Calculator.divide(81, 9)).toEqual(9);
+});
+
+test('divide() retruns a correct division of input integers(fractional division)', () => {
+    expect(Calculator.divide(5, 10)).toEqual(0.5);
+});
+
+test('divide() retruns a correct division of input integers(fractional division)', () => {
+    expect(Calculator.divide(9, 81)).toEqual(0.11);
+});
+
+test('divide() retruns a correct division of input integers(fractional division)', () => {
+    expect(Calculator.divide(2, 3)).toEqual(0.67);
+});
+
+test('divide() throws an Error on division by zero', () => {
+    expect(() => Calculator.divide(2, 0)).toThrow('a division by zero is impossible');
+});
+
+test('add() throws a type Error on input that is not numeric', () => {
+    expect(() => Calculator.add('string', 2675)).toThrow('wrong type, please input a numeric value');
+});
+
+test('add() throws a type Error on input that is not numeric', () => {
+    expect(() => Calculator.add(Infinity, 2675)).toThrow('wrong type, please input a numeric value');
+});
+
+test('multiply() throws a type Error on input that is not numeric', () => {
+    expect(() => Calculator.multiply(null, undefined)).toThrow('wrong type, please input a numeric value');
+});
+
+test('multiply() throws a type Error on input that is not numeric', () => {
+    expect(() => Calculator.multiply(NaN, 22)).toThrow('wrong type, please input a numeric value');
+});
+
+test('divide() throws a type Error on input that is not numeric', () => {
+    expect(() => Calculator.divide('a', 'b')).toThrow('wrong type, please input a numeric value');
+});
+
+test('substract() throws a type Error on input that is not numeric', () => {
+    expect(() => Calculator.substract(1, 'dfa')).toThrow('wrong type, please input a numeric value');
+});
