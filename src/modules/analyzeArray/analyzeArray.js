c065232af0d730ce761cba8e11ecbda85e0c0980
Pontificust <lihtinmann88@yandex.ru>
2026-09-03 11:20:49 +0300

    feat: the analyzeArray() is tested and implemented

---
+++ src/modules/analyzeArray/analyzeArray.js
@@ -0,0 +1,20 @@
+export const analyzeArray = (arr) => {
+    if(!Array.isArray(arr) || arr.length === 0) {
+        throw new Error('wrong type, please input an array of numeric values');
+    }
+
+    arr.forEach(val => {
+        if(!Number.isFinite(val)) {
+            throw new Error('wrong type, please input an array of numeric values');
+        }
+    });
+
+  return {
+    average:
+      Math.round((arr.reduce((acc, val) => acc + val, 0) / arr.length) * 100) /
+      100,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
+  };
+};
